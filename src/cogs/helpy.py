6014f0b56910e7b6ce41aa8c2263576d3796cd47
omoNattie <98173891+omoNattie@users.noreply.github.com>
2022-05-21 18:27:13 +0300

    small changes

--- src/cogs/helpy.py
+++ src/cogs/helpy.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+
+
 # imports
 
 
@@ -11,17 +13,19 @@ class Help(commands.Cog):
         async def help(ctx):
             helpemb = discord.Embed(color=discord.Color.green())
             helpemb.add_field(name="Commands",
-                              value="t!owo\nt!help\nt!uwu\nt!tubbo)\nt!pfps\nt!banish\nt!eradicate\nt!transrate")
+                              value="t!owo\nt!help\nt!uwu\nt!tubbo(Inside joke, we are not DreamSMP "
+                                    "simps)\nt!pfps\nt!banish\nt!eradicate\nt!transrate")
             helpemb.add_field(name="About Commands", value="t!owo - About command\nt!help - This command, talks "
-                                                           "about the bots commands\nt!uwu - uwuifies your "
-                                                           "text\nt!tubbo - Changes your name to \"Tubbo from "
-                                                           "Dream\"\nt!pfps - Gets your or a tagged member's profile "
-                                                           "picture\nt!eradicate - kick tagged member, structure is "
-                                                           "t!eradicate (member) (reason)\nt!banish - Bans tagged "
-                                                           "member, structure is t!banish (member) ("
-                                                           "reason)\nt!transrate - Tells you on a percent scale how "
-                                                           "trans you are",
-                              inline=False)
+                                                           "about the bots commands", inline=False)
+            helpemb.add_field(name="Moderation Commands", value="t!eradicate - Kicks tagged member, structure is "
+                                                                "t!eradicate (member) (reason)\nt!banish - Bans tagged "
+                                                                "member, structure is\nt!banish (member) ("
+                                                                "reason)", inline=False)
+            helpemb.add_field(name="Fun Commands", value="t!uwu - uwuifies your "
+                                                         "text\nt!tubbo - Changes your name to \"Tubbo from "
+                                                         "Dream\"\nt!pfps - Gets your or a tagged member's profile "
+                                                         "picture\nt!transrate - Tells you on a percent scale how "
+                                                         "trans you are", inline=False)
             await ctx.send(
                 embed=helpemb
             )
