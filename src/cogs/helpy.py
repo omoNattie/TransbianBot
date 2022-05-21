d22328c47b1dd689552964c0fb1ed2194aba780a
omoNattie <98173891+omoNattie@users.noreply.github.com>
2022-05-21 05:02:24 +0300

    test.py has been replaced with moderation.py, and added ban and kick command under the name of banish. updated help command

--- src/cogs/helpy.py
+++ src/cogs/helpy.py
@@ -10,12 +10,15 @@ class Help(commands.Cog):
         async def helps(ctx):
             helpemb = discord.Embed(color=discord.Color.green())
             helpemb.add_field(name="Commands",
-                              value="t!owo\nt!helps\nt!uwu\nt!tubbo)\nt!pfps")
+                              value="t!owo\nt!helps\nt!uwu\nt!tubbo)\nt!pfps\nt!banish\nt!eradicate")
             helpemb.add_field(name="About Commands", value="t!owo - About command\nt!helps - This command, talks "
                                                            "about the bots commands\nt!uwu - uwuifies your "
                                                            "text\nt!tubbo - Changes your name to \"Tubbo from "
                                                            "Dream\"\nt!pfps - Gets your or a tagged member's profile "
-                                                           "picture", inline=False)
+                                                           "picture\nt!eradicate - bans tagged member, structure is "
+                                                           "t!eradicate (member) (reason)\nt!banish - bans tagged "
+                                                           "member, structure is t!banish (member) (reason)",
+                              inline=False)
             await ctx.send(
                 embed=helpemb
             )
