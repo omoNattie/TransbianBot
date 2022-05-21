d31da68a60c0d0a6b731f715a9b491d0d43fa364
omoNattie <98173891+omoNattie@users.noreply.github.com>
2022-05-21 19:05:27 +0300

    Fixed error where missing permissions wouldn't pop up

--- src/cogs/moderation.py
+++ src/cogs/moderation.py
@@ -11,7 +11,7 @@ class KickPeople(commands.Cog):
         @has_permissions(kick_members=True)  # check if member has permission to kick
         async def eradicate(ctx, m: discord.Member = None, *, reason=None):
             if reason is None:
-                reason = "No reason has been provided"  # if no reason provided send
+                reason = "No reason has been provided"  # if no reason provided show
 
             if m is None:
                 await ctx.send("Who am I supposed to kick? You tagged no one!")  # if no one is tagged send
@@ -21,10 +21,10 @@ class KickPeople(commands.Cog):
                 await ctx.guild.kick(m)  # kick member
                 await ctx.send(f"{m} has been eradicated for {reason}!")
 
-        @eradicate.error  # if permissions are missing send (not working for some reason, doesn't throw any errors)
-        async def banish_error(error, ctx):
+        @eradicate.error  # if permissions are missing send
+        async def banish_error(ctx, error):
             if isinstance(error, commands.MissingPermissions):
-                await ctx.channel.send("I'm sorry, but this command is made for admins!")
+                await ctx.send("I'm sorry, but this command is made for admins!")
 
 
 class BanPeople(commands.Cog):
@@ -35,20 +35,20 @@ class BanPeople(commands.Cog):
         @has_permissions(ban_members=True)  # check if member can ban people
         async def banish(ctx, m: discord.Member = None, *, reason=None):
             if reason is None:
-                reason = "No reason has been provided"  # if no reason provided send
+                reason = "No reason has been provided"  # if no reason provided change
 
             if m is None:
-                await ctx.send(f"I can't ban air, {ctx.author}!")  # if no one is pinged send this
-            elif m == ctx.author:
-                await ctx.send(f"Nuu You can't ban yourself, {ctx.author}!")  # if author pinged themselves send this
+                await ctx.send(f"I can't ban air, {ctx.author.name}!")  # if no one is pinged send this
+            elif m == ctx.author:  # if auth pinged themselves send
+                await ctx.send(f"Nuu You can't ban yourself, {ctx.author.name}!")
             else:
                 await m.ban()  # ban pinged member
                 await ctx.send(f"{m} has been sent to the Elysian Plain for {reason}!")  # after ban send this
 
         @banish.error
-        async def eradicate_error(error, ctx):
+        async def eradicate_error(ctx, error):
             if isinstance(error, commands.MissingPermissions):
-                await ctx.channel.send("I'm sorry, but this command is made for admins!")
+                await ctx.send("I'm sorry, but this command is made for admins!")
 
 
 def setup(client):
