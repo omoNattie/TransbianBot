d89d0ea07de7220325afa8c5a4a7b7b65158b114
omoNattie <98173891+omoNattie@users.noreply.github.com>
2022-05-21 06:52:45 +0300

    small changes

--- src/cogs/helpy.py
+++ src/cogs/helpy.py
@@ -10,14 +10,16 @@ class Help(commands.Cog):
         async def help(ctx):
             helpemb = discord.Embed(color=discord.Color.green())
             helpemb.add_field(name="Commands",
-                              value="t!owo\nt!help\nt!uwu\nt!tubbo)\nt!pfps\nt!banish\nt!eradicate")
+                              value="t!owo\nt!help\nt!uwu\nt!tubbo)\nt!pfps\nt!banish\nt!eradicate\nt!transrate")
             helpemb.add_field(name="About Commands", value="t!owo - About command\nt!help - This command, talks "
                                                            "about the bots commands\nt!uwu - uwuifies your "
                                                            "text\nt!tubbo - Changes your name to \"Tubbo from "
                                                            "Dream\"\nt!pfps - Gets your or a tagged member's profile "
-                                                           "picture\nt!eradicate - bans tagged member, structure is "
-                                                           "t!eradicate (member) (reason)\nt!banish - bans tagged "
-                                                           "member, structure is t!banish (member) (reason)",
+                                                           "picture\nt!eradicate - kick tagged member, structure is "
+                                                           "t!eradicate (member) (reason)\nt!banish - Bans tagged "
+                                                           "member, structure is t!banish (member) ("
+                                                           "reason)\nt!transrate - Tells you on a percent scale how "
+                                                           "trans you are",
                               inline=False)
             await ctx.send(
                 embed=helpemb
