f9f36c70b7208db026cf1fc45d5bf6e3ed571b5b
omoNattie <98173891+omoNattie@users.noreply.github.com>
2022-05-26 21:07:08 +0300

    tubbo gone

--- src/cogs/helpy.py
+++ src/cogs/helpy.py
@@ -13,18 +13,18 @@ class Help(commands.Cog):
         async def help(ctx):
             helpemb = discord.Embed(color=discord.Color.green())
             helpemb.add_field(name="Commands",
-                              value="t!owo\nt!help\nt!uwu\nt!tubbo(Inside joke, we are not DreamSMP "
-                                    "simps)\nt!pfp\nt!banish\nt!eradicate\nt!transrate\nt!celeste\nt!transmeme\nt"
-                                    "!repel,\nt!serverpicture")
+                              value="t!owo\nt!help\nt!uwu\nt!pfp\nt!banish\nt!eradicate\nt!transrate\nt!celeste\nt"
+                                    "!transmeme\nt"
+                                    "!repel,\nt!serverpicture\nt!roll")
             helpemb.add_field(name="About Commands", value="t!owo - About command\nt!help - This command, talks "
                                                            "about the bots commands", inline=False)
             helpemb.add_field(name="Moderation Commands", value="t!eradicate - Kicks tagged member, structure is "
-                                                                "t!eradicate (member) (reason)\nt!banish - Bans tagged "
+                                                                "\nt!eradicate (member) (reason)\nt!banish - Bans "
+                                                                "tagged "
                                                                 "member, structure is\nt!banish (member) ("
                                                                 "reason)", inline=False)
             helpemb.add_field(name="Fun Commands", value="t!uwu - uwuifies your "
-                                                         "text\nt!tubbo - Changes your name to \"Tubbo from "
-                                                         "Dream\"\nt!pfp - Gets your or a tagged member's profile "
+                                                         "text\nt!pfp - Gets your or a tagged member's profile "
                                                          "picture\nt!transrate - Tells you on a percent scale how "
                                                          "trans you are, you can also tag a member to see how trans "
                                                          "*they* are!\nt!celeste - Sends a random post from "
@@ -32,7 +32,8 @@ class Help(commands.Cog):
                                                          "r/traaaaaaannnnnnnnnns\nt!repel - Repel someone by tagging "
                                                          "them! Or don't tag anyone and let them "
                                                          "guess!\nt!serverpicture - Gets the server picture and sends "
-                                                         "it", inline=False)
+                                                         "it\nt!roll - Rolls a number 1-20 for anything you'd like, "
+                                                         "structure is\nt!roll (roll reason)", inline=False)
             await ctx.send(
                 embed=helpemb
             )
